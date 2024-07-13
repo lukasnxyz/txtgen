44f9fb8cfba0c3da57f38d635640650178cd26ce
lukasnxyz <lukasnitzsche@yahoo.com>
2024-07-13 11:29:41 -0500

    clean up, prepare_data.py for multiple datasets

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 * Building a karpathy NanoGPT like model
 * Implementing [Attention Is All You Need](https://arxiv.org/abs/1706.03762)
 * Character-level language model
-* Common words data set: http://norvig.com/google-books-common-words.txt
+
+### Quick start
+```bash
+python3 -m venv venv
+pip3 install -r requirements.txt
+```
+download datasets
+```bash
+python3 prepare_data.py
+```
--- prepare_data.py
+++ prepare_data.py
@@ -1,15 +1,37 @@
 import os
 import requests
 
-data_dir = 'data'
+print('choose data to download:')
+print('0. names\n1. tiny_shakespeare\n2. truth_sentences')
+choice = int(input('> '))
 
-# make data dir
-if not os.path.exists(data_dir):
+data_dir = 'data/'
+if not os.path.exists(data_dir): 
+    print(f'making dir: {data_dir}')
     os.makedirs(data_dir)
 
-# get shakespeare data
-input_file_path = os.path.join(data_dir, 'input.txt')
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    with open(input_file_path, 'w', encoding='utf-8') as f:
-        f.write(requests.get(data_url).text)
+urls = ['https://raw.githubusercontent.com/karpathy/makemore/master/names.txt',
+        'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt',
+        'https://gist.githubusercontent.com/lukasnxyz/d7b29398dd3b3d1c1dcb14f1f19e3744/raw/8157e9b21d4169d39a99bd8e27287a5bc44de31a/truth_sentences.txt']
+
+match choice:
+    case 0: 
+        in_path = os.path.join(data_dir, 'names.txt')
+        if not os.path.exists(in_path):
+            with open(in_path, 'w', encoding='utf-8') as f: 
+                print(f'downloading {urls[choice]}')
+                f.write(requests.get(urls[choice]).text)
+    case 1: 
+        in_path = os.path.join(data_dir, 'tiny_shakespeare.txt')
+        if not os.path.exists(in_path):
+            with open(in_path, 'w', encoding='utf-8') as f: 
+                print(f'downloading {urls[choice]}')
+                f.write(requests.get(urls[choice]).text)
+    case 2: 
+        in_path = os.path.join(data_dir, 'truth_sentences.txt')
+        if not os.path.exists(in_path):
+            with open(in_path, 'w', encoding='utf-8') as f: 
+                print(f'downloading {urls[choice]}')
+                f.write(requests.get(urls[choice]).text)
+    case _:
+        print(f'input {choice} is invalid!')
